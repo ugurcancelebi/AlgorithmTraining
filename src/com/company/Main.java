38ca6824d1d6a09af2d7b8acca8c5f4895cbf6af
ugurcancelebi <42876704+ugurcancelebi@users.noreply.github.com>
2019-09-13 11:56:22 +0300

    Added toString method to console.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.java.swing.plaf.windows.WindowsInternalFrameTitlePane;
+
 import java.util.Scanner;
 
 public class Main {
@@ -54,19 +56,34 @@ public class Main {
              sonuc *= i;
          }System.out.println(sonuc);
     }
-    private static void decreaseAndIncrease(){
-        int num = new Scanner(System.in).nextInt();
-        for (int i = num;i>0;i--){
-            System.out.print(i);
-            for (int j=i-1 ;j>0;j--){
-                System.out.print(j);
-            }
-            for (int k=2 ;k<=i;k++){
-                System.out.print(k);
-            }
-            System.out.print("\n");
-        }
+    private static String parse(int number){
+        int first,second,third;
+        String firstChar = "",secondChar= "",thirdChar = "";
+        first = number%10;
+        second = (number%100)/10;
+        third = (number%1000)/100;
+        firstChar = getString(first, firstChar);
+        secondChar = getString(second, secondChar);
+        thirdChar = getString(third, thirdChar);
+        return thirdChar+secondChar+firstChar;
     }
+
+    private static String getString(int first, String character) {
+        switch (first){
+            case 1: character = "1"; break;
+            case 2: character = "2"; break;
+            case 3: character = "3"; break;
+            case 4: character = "4"; break;
+            case 5: character = "5"; break;
+            case 6: character = "6"; break;
+            case 7: character = "7"; break;
+            case 8: character = "8"; break;
+            case 9: character = "9"; break;
+            case 0: character = "0"; break;
+        }
+        return character;
+    }
+
     private static void primeNumberList(){
         int count;
         int sayi = 0;
@@ -90,12 +107,32 @@ public class Main {
             num++;
         }
     }
+
+    private static void ChangetoString(){
+        System.out.println("Please define a variable to parse..");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Parsed value is : '"+ parse(number)+"' debug to see resutls.");
+    }
+
+    private static void decreaseAndIncrease(){
+        int num = new Scanner(System.in).nextInt();
+        for (int i = num;i>0;i--){
+            System.out.print(i);
+            for (int j=i-1 ;j>0;j--){
+                System.out.print(j);
+            }
+            for (int k=2 ;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.print("\n");
+        }
+    }
     public static void main(String[] args) {
         int choice;
         do {
             System.out.println("Algorithm Console \n Please Select the operation you want to use \n" +
                     " 1.)Check Polyndrome \n 2.)Total digit of number \n 3.)Star Application \n 4.)Increase and Decrease" +
-                    " \n 5.)Prime Number List \n 6.)Factorial \n 7.)Exit ");
+                    " \n 5.)Prime Number List \n 6.)Factorial \n 7.)Parse \n 8.)Exit ");
             choice = new Scanner(System.in).nextInt();
             switch (choice) {
                 case 1:
@@ -117,11 +154,13 @@ public class Main {
                     factorial();
                     break;
                 case 7:
+                    ChangetoString();
+                    break;
+                case 8:
                     System.out.println("So long!");
                     System.exit(0);
                     break;
             }
-        }while (choice>=1 && choice<=7);
+        }while (choice>=1 && choice<=8);
     }
 }
-
