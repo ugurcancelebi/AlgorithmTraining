daeb56cb41b09eb363fd0a492abfdeeced8cbbf2
ugurcancelebi <42876704+ugurcancelebi@users.noreply.github.com>
2019-09-14 21:14:51 +0300

    Added toString method to console.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import com.sun.java.swing.plaf.windows.WindowsInternalFrameTitlePane;
-
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +12,6 @@ public class Main {
         }
         return word.equals(newWord.toString());
     }
-
     private  static void checkPolyndrome(){
         String word;
         Scanner scanner = new Scanner(System.in);
@@ -24,7 +21,6 @@ public class Main {
             System.out.println("Polyndrome word");
         } else System.out.println("Non polyndrome word");
     }
-
     private static void digitTotal(){
         int num,first,second,third;
         Scanner in = new Scanner(System.in);
@@ -48,13 +44,13 @@ public class Main {
         }
     }
     private static void factorial(){
-         int sonuc = 1;
-         int sayi;
-         System.out.println("Sayi giriniz.");
-         sayi = new Scanner(System.in).nextInt();
-         for (int i = 1;i<=sayi;i++) {
-             sonuc *= i;
-         }System.out.println(sonuc);
+         int result = 1;
+         int number;
+         System.out.println("Input an integer.");
+        number = new Scanner(System.in).nextInt();
+         for (int i = 1;i<=number;i++) {
+             result *= i;
+         }System.out.println(result);
     }
     private static String parse(int number){
         int first,second,third;
@@ -67,7 +63,6 @@ public class Main {
         thirdChar = getString(third, thirdChar);
         return thirdChar+secondChar+firstChar;
     }
-
     private static String getString(int first, String character) {
         switch (first){
             case 1: character = "1"; break;
@@ -83,7 +78,18 @@ public class Main {
         }
         return character;
     }
-
+    private static void ChangetoString(){
+        System.out.println("Please define a variable to parse..");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Parsed value is : '"+ parse(number)+"' debug to see resutls.");
+    }
+    private static void convertToBinary(){
+        int number = new Scanner(System.in).nextInt();
+        StringBuilder decimalString = new StringBuilder();
+        for(int i = number;i>0;i/=2){
+            decimalString.append(number % 2);
+        }System.out.println(decimalString);
+    }
     private static void primeNumberList(){
         int count;
         int sayi = 0;
@@ -107,13 +113,6 @@ public class Main {
             num++;
         }
     }
-
-    private static void ChangetoString(){
-        System.out.println("Please define a variable to parse..");
-        int number = new Scanner(System.in).nextInt();
-        System.out.println("Parsed value is : '"+ parse(number)+"' debug to see resutls.");
-    }
-
     private static void decreaseAndIncrease(){
         int num = new Scanner(System.in).nextInt();
         for (int i = num;i>0;i--){
@@ -127,12 +126,51 @@ public class Main {
             System.out.print("\n");
         }
     }
+    private static void rockPaperScissors(){
+        System.out.println("Please select your option : \n 1.) Rock \n 2.) Paper \n 3.) Scissors \n");
+        int userChoice = new Scanner(System.in).nextInt()-1;
+        caseMethod(userChoice);
+        try {
+            for (int i = 3;i>=0;i--) {
+                Thread.sleep(1000);
+                System.out.println("Please wait for " + i + " seconds for computer's choice ");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int randomComputerChoice = (int)(Math.random()*3);
+        caseMethod(randomComputerChoice);
+        if (caseMethod(userChoice) == caseMethod(randomComputerChoice)){
+            System.out.println("Draw");
+        }else if ((caseMethod(userChoice) - caseMethod(randomComputerChoice) == 1)
+                    || (caseMethod(userChoice) - caseMethod(randomComputerChoice) == -2)){
+            System.out.println("You Win !");
+        }else {
+            System.out.println("Computer Wins !");
+        }
+    }
+
+    private static int caseMethod(int userChoice) {
+        switch (userChoice){
+            case 0:
+                System.out.println("Rock");
+                break;
+            case 1:
+                System.out.println("Paper");
+                break;
+            case 2:
+                System.out.println("Scissors");
+                break;
+        }
+        return userChoice;
+    }
+
     public static void main(String[] args) {
-        int choice;
+       int choice;
         do {
             System.out.println("Algorithm Console \n Please Select the operation you want to use \n" +
-                    " 1.)Check Polyndrome \n 2.)Total digit of number \n 3.)Star Application \n 4.)Increase and Decrease" +
-                    " \n 5.)Prime Number List \n 6.)Factorial \n 7.)Parse \n 8.)Exit ");
+                    " 1.)Check Polyndrome \n 2.)Total digit of number (3 Digits Max.) \n 3.)Star Application \n 4.)Increase and Decrease" +
+                    " \n 5.)Prime Number List \n 6.)Factorial \n 7.)Parse (3 Digits Max.) \n 8.)Convert to Binary \n 9.)Rock,Paper & Scissors \n 10.)Exit ");
             choice = new Scanner(System.in).nextInt();
             switch (choice) {
                 case 1:
@@ -157,10 +195,16 @@ public class Main {
                     ChangetoString();
                     break;
                 case 8:
+                    convertToBinary();
+                    break;
+                case 9:
+                    rockPaperScissors();
+                    break;
+                case 10:
                     System.out.println("So long!");
                     System.exit(0);
                     break;
             }
-        }while (choice>=1 && choice<=8);
+        } while (choice>=1 && choice<=10);
     }
 }
