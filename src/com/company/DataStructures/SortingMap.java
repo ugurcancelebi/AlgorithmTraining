eae8b0db24680882b5721871aa9721a4996088cc
ugurcancelebi <42876704+ugurcancelebi@users.noreply.github.com>
2020-01-10 09:15:37 +0300

    Java map components used in project to order list items

---
+++ src/com/company/DataStructures/SortingMap.java
@@ -0,0 +1,41 @@
+package com.company.DataStructures;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Compare implements Comparator{
+    private Map<String,Double> map;
+    Compare(Map<String,Double> map1) {
+        this.map = map1;
+    }
+    @Override
+    public int compare(Object o, Object t1) {
+        return map.get(o).compareTo(map.get(t1));
+    }
+}
+public class SortingMap {
+    public static void main(String[] args) {
+        HashMap<String ,Double> hashMap = new HashMap<>();
+        Compare comparator = new Compare(hashMap);
+        hashMap.put("FirstVal",1.0);
+        hashMap.put("ThirdVal",3.0);
+        hashMap.put("SecondVal",2.0);
+        System.out.println("Unordered MAP : ");
+        for (String key : hashMap.keySet()){
+            System.out.print("Key : " + hashMap.get(key) + " Val : " + key);
+        }
+        TreeMap<String,Double> sortingTree = new TreeMap<String ,Double>(comparator);
+        sortingTree.putAll(hashMap);
+        System.out.println("\nOrdered MAP :");
+        for (String key : sortingTree.keySet()){
+            System.out.print("Key : "+ sortingTree.get(key) + " Val : " + key);
+        }
+        /*System.out.println("Key : + " + hashMap.keySet() + "Val :" +  hashMap.values());
+        TreeMap<String, Double> sortingTree = new TreeMap<>(hashMap);
+        for (String key: hashMap.keySet()){
+            System.out.println("Key : " + hashMap.values() + "Val : " +  key);
+        }*/
+    }
+}
