a280df4a732ab88d0eba32569d1d421a6969e144
ucelebi <ugurcancelebi@amadeus.com>
2020-06-12 20:42:01 +0300

    MD5 Encryption Algorithm

---
+++ src/com/company/AlgorithmTraining/MD5Encryption.java
@@ -0,0 +1,27 @@
+package com.company.AlgorithmTraining;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Encryption {
+    public static void main(String[] args)
+    {
+        String passwordToHash = "password";
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(passwordToHash.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println(generatedPassword);
+    }
+}
